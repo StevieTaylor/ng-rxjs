4a2771eef8c8ad49d5f40f1cdadd97e33e5b43ef
StevieTaylor <galei111@126.com>
2021-07-07 18:12:16 +0800

    [feat] utility operators

--- src/app/pages/operators/utility/utility.component.ts
+++ src/app/pages/operators/utility/utility.component.ts
@@ -1,4 +1,13 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-07-02 17:59:06
+ * @LastEditTime: 2021-07-07 14:59:25
+ * @LastEditors: Stevie
+ * @Description: 
+ */
 import { Component, OnInit } from '@angular/core';
+import { IOperator } from './../../../shared/models/operator';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-utility',
@@ -7,9 +16,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UtilityComponent implements OnInit {
 
+  public utilityOperators: IOperator[] = [
+    {
+      key: 1,
+      name: 'do',
+      signature: '',
+      description: ''
+    },
+    {
+      key: 2,
+      name: 'delay',
+      signature: '',
+      description: ''
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    this.useDo();
   }
 
+  public useOperators(operatorName: string) {
+
+  }
+
+  private useDo() {
+    const source = of(1, 2, 3, 4, 5);
+    console.log(`source`, source)
+    source.subscribe((res) => {
+      console.log(`res`, res)
+    })
+  }
 }
---
+++ src/app/shared/models/operator.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-07-07 14:53:47
+ * @LastEditTime: 2021-07-07 14:54:35
+ * @LastEditors: Stevie
+ * @Description:
+ */
+export interface IOperator {
+  key: string | number;
+  name: string;
+  signature: string;
+  description: string;
+}
