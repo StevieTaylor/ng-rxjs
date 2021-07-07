3d1091801884417f0e3e4588aee31d9db6cdafc3
StevieTaylor <galei111@126.com>
2021-07-07 11:48:20 +0800

    [feat] 路由激活时展开对应菜单栏

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-layout",
@@ -68,23 +68,15 @@ export class LayoutComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    // this.getRouteInfo();
+    this.activatedRoute();
   }
 
-  public getRouteInfo() {
+  public activatedRoute() {
     const { url } = this.router;
-    console.log(this.hasActivatedRoute(this.menus, url));
-  }
-
-  public hasActivatedRoute(menus: IMenu[], url: string) {
-    for (let i = 0; i < menus.length; i++) {
-      const menu = menus[i];
-      if (!menu.children) {
-        console.log(`url`, url)
-        console.log(`menu.link`, menu.link)
-        return url.includes(menu.link);
-      } else {
-        this.hasActivatedRoute(menu.children, url);
+    for (let i = 0; i < this.menus.length; i++) {
+      const menu = this.menus[i];
+      if (menu.children) {
+        this.menus[i].isOpen = menu.children.find((submenu: IMenu) => url.includes(submenu.link)) ? true : false
       }
     }
   }
