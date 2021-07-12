4f4d101da241ec47469432b5203775e27959b739
StevieTaylor <galei111@126.com>
2021-07-12 19:03:47 +0800

    [feat] Utility operators

--- src/app/pages/operators/utility/utility.component.ts
+++ src/app/pages/operators/utility/utility.component.ts
@@ -1,15 +1,23 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-02 17:59:06
- * @LastEditTime: 2021-07-08 15:41:43
+ * @LastEditTime: 2021-07-12 15:52:06
  * @LastEditors: Stevie
  * @Description: utility
  */
 import { Component, OnInit } from "@angular/core";
 import { IOperator } from "./../../../shared/models/operator";
 import { firstUpperCase } from "src/app/shared/utils/utils";
-import { of, merge } from "rxjs";
-import { mapTo, delay } from "rxjs/operators";
+import { of, merge, fromEvent, interval } from "rxjs";
+import {
+  mapTo,
+  tap,
+  delay,
+  map,
+  debounceTime,
+  timeInterval,
+  timeout,
+} from "rxjs/operators";
 
 @Component({
   selector: "app-utility",
@@ -20,13 +28,27 @@ export class UtilityComponent implements OnInit {
   public utilityOperators: IOperator[] = [
     {
       key: 1,
-      name: "do",
-      signature: "",
-      description: "",
+      name: "tap",
+      signature:
+        "tap(nextOrObserver: function, error: function, complete: function): Observable",
+      description: "透明地执行操作或副作用，比如打印日志",
     },
     {
       key: 2,
       name: "delay",
+      signature:
+        "delay(delay: number | Date, scheduler: Scheduler): Observable",
+      description: "按照指定时间之后发出值",
+    },
+    {
+      key: 3,
+      name: "timeInterval",
+      signature: "timeInterval(scheduler: SchedulerLike): Observable<TimeInterval<any>> | WebSocketSubject<T> | Observable<T>",
+      description: "",
+    },
+    {
+      key: 4,
+      name: "timeout",
       signature: "",
       description: "",
     },
@@ -46,26 +68,45 @@ export class UtilityComponent implements OnInit {
     }
   }
 
-  useDo() {
-    const source$ = of(1, 2, 3, 4, 5);
-    source$.subscribe((value) => {
-      console.log(value);
+  useTap() {
+    const clicks$ = fromEvent(document, "click");
+    const postions = clicks$.pipe(
+      tap((e: MouseEvent) => console.log("event:", e)),
+      map((e: MouseEvent) => e.clientX),
+      debounceTime(500)
+    );
+    postions.subscribe((value) => {
+      console.log("clientX:", value);
     });
   }
 
   useDelay() {
     const source$ = of(null);
     const message = merge(
-      source$.pipe(delay(1000), mapTo("hello")),
-      source$.pipe(delay(2000), mapTo("ni")),
-      source$.pipe(delay(3000), mapTo("hao"))
+      source$.pipe(mapTo("delay 1s"), delay(1000)),
+      source$.pipe(mapTo("delay 2s"), delay(2000)),
+      source$.pipe(mapTo("delay 3s"), delay(3000))
     );
     message.subscribe((value) => {
       console.log(value);
     });
   }
 
-  useToPromise() {
+  useTimeInterval() {
+    const events$ = fromEvent(document, "mousedown");
+    events$
+      .pipe(timeInterval(), tap(console.log))
+      .subscribe((value: { interval: number; value: MouseEvent }) =>
+        console.log("time interval before last click", value.interval)
+      );
+  }
 
+  useTimeout() {
+    const seconds$ = interval(1000);
+    seconds$.pipe(
+      timeout(5000)
+    ).subscribe(
+      value => console.log(value)
+    )
   }
 }
--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Stevie
  * @Date: 2021-07-08 10:13:19
- * @LastEditTime: 2021-07-08 10:14:15
+ * @LastEditTime: 2021-07-12 15:20:37
  * @LastEditors: Stevie
  * @Description:
  */
 export function firstUpperCase(str: string) {
-  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+  return str.replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
 }
